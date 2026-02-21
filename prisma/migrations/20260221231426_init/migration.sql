ef1af04de48c131e5fe9adaa81ba72149ec0ff40
Hidalgo87 <juanfgomez6@gmail.com>
2026-02-21 18:42:50 -0500

    ➕ Add Prisma schema, migration files, and Prisma service for database integration

---
+++ prisma/migrations/20260221231426_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "Director" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Director_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "releaseYear" INTEGER NOT NULL,
+    "directorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "movieId" INTEGER NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Movie" ADD CONSTRAINT "Movie_directorId_fkey" FOREIGN KEY ("directorId") REFERENCES "Director"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
