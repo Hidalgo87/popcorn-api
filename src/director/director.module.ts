be4c71edaeb91b6504b319465a087cea3dc81a1a
Hidalgo87 <juanfgomez6@gmail.com>
2026-04-14 16:34:32 -0500

    ✨ feat: add ExternalApiService for handling employee creation and update DirectorModule to include it

--- src/director/director.module.ts
+++ src/director/director.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DirectorService } from './director.service';
 import { DirectorController } from './director.controller';
+import { ExternalApiService } from 'src/external_api/externalApi.service';
 
 @Module({
   controllers: [DirectorController],
-  providers: [DirectorService],
+  providers: [DirectorService, ExternalApiService],
 })
 export class DirectorModule {}
---
+++ src/external_api/externalApi.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { randomInt } from 'crypto';
+import { CreateDirectorDto } from 'src/director/dto/create-director.dto';
+
+@Injectable()
+export class ExternalApiService {
+  async createEmployee(createDirectorDto: CreateDirectorDto) {
+    const fullJson = {
+      cedula: randomInt(10000000, 99999999),
+      nombre: createDirectorDto.name,
+      rol: 'Director',
+      jsonDirector: {
+        nombre: createDirectorDto.name,
+        jsonDoctor: { ...createDirectorDto.jsonDoctor },
+      },
+    };
+    const res = await fetch(
+      'http://apidevops-prod.eba-hiqy8jtp.us-east-2.elasticbeanstalk.com/api/v2/empleados',
+      {
+        method: 'POST',
+        body: JSON.stringify(fullJson),
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+
+    if (!res.ok) {
+      console.log('res', res);
+      throw new Error('External API error');
+    }
+
+    return fullJson;
+  }
+}
